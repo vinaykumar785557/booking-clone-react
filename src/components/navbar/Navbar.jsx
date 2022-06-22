45b106b1c6f0d15e75b626ca638ca4aa8632ae2c
vinaykumar785557 <c0785557@mylambton.ca>
2022-06-21 23:15:27 -0400

    finished project

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './navbar.css';
+import { Link } from 'react-router-dom';
+function Navbar() {
+	return (
+		<div className='navbar'>
+			<div className='navContainer'>
+				<Link to={'/'} style={{ color: 'white', textDecoration: 'none' }}>
+					<span className='logo'>Booking.com</span>
+				</Link>
+
+				<div className='navItems'>
+					<button className='navButton'>Register</button>
+					<button className='navButton'>Login</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,99 @@
+import './list.css';
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { DateRange } from 'react-date-range';
+import SearchItem from '../../components/searchItem/SearchItem';
+function List() {
+	const location = useLocation();
+	// console.log(location);
+
+	const [destination, setDestination] = useState(location.state.destination);
+	const [date, setDate] = useState(location.state.date);
+	const [options, setOptions] = useState(location.state.options);
+	const [openDate, setOpenDate] = useState(false);
+	return (
+		<div>
+			<Navbar />
+			<Header type='list' />
+			<div className='listContainer'>
+				<div className='listWrapper'>
+					<div className='listSearch'>
+						<h1 className='lsTitle'>Search</h1>
+						<div className='lsItem'>
+							<label>Destination</label>
+							<input type='text' placeholder={destination} />
+						</div>
+						<div className='lsItem'>
+							<label>Check-in Date</label>
+							<span onClick={() => setOpenDate(!openDate)}>{`${format(
+								date[0].startDate,
+								'MM/dd/yyyy'
+							)} to ${format(date[0].endDate, 'MM/dd/yyyy')}  `}</span>
+							{openDate && (
+								<DateRange
+									onChange={(item) => setDate([item.selection])}
+									ranges={date}
+									minDate={new Date()}
+								/>
+							)}
+						</div>
+
+						<div className='lsItem'>
+							<label>Options</label>
+							<div className='lsOptions'>
+								<div className='lsOptionItem'>
+									<span className='lsOptionText'>
+										Min Price <small>per night</small>
+									</span>
+									<input type='number' className='lsOptionInput' />
+								</div>
+								<div className='lsOptionItem'>
+									<span className='lsOptionText'>
+										Max Price <small>per night</small>
+									</span>
+									<input type='number' className='lsOptionInput' />
+								</div>
+								<div className='lsOptionItem'>
+									<span className='lsOptionText'>Adult</span>
+									<input
+										min={1}
+										type='number'
+										className='lsOptionInput'
+										placeholder={options.adult}
+									/>
+								</div>
+								<div className='lsOptionItem'>
+									<span className='lsOptionText'>Children</span>
+									<input
+										min={0}
+										type='number'
+										className='lsOptionInput'
+										placeholder={options.children}
+									/>
+								</div>
+								<div className='lsOptionItem'>
+									<span className='lsOptionText'>Room</span>
+									<input
+										min={1}
+										type='number'
+										className='lsOptionInput'
+										placeholder={options.room}
+									/>
+								</div>
+							</div>
+						</div>
+						<button>Search</button>
+					</div>
+					<div className='listResult'>
+						<SearchItem />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default List;
